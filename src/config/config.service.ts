623b64d40abc245a76a03877dc4c79a5527af59b
Andersoniza <ajc.iza@yavirac.edu.ec>
2024-11-11 21:16:42 -0500

    Clase10-11

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,18 +4,19 @@ import { parse } from "dotenv";
 
 @Injectable()
 export class ConfigService {
-  private readonly envConfig: { [key: string]: string };
+  private readonly envConfig: { [key: string]: string }
   constructor() {
-    const env =process.env.NODE_ENV || 'development'
-    const envFilePath = ` ${__dirname}/../../../.env.${env}`;
-    const existsPath = fs.existsSync(envFilePath);
-    if (!existsPath) {
-      console.log(` .env. ${process.env.NODE_ENV} no existe`);
-      process.exit(0);
-    }
+    const env = process.env.NODE_ENV || 'development'
+    const envFilePath = `${__dirname}/../../../.env.${env}`
+    const existsPath = fs.existsSync(envFilePath)
+        if(!existsPath) {
+          console.log(`.env.${process.env.NODE_ENV} no existe`);
+          process.exit(0);
+        }
+        this.envConfig = parse(fs.readFileSync(envFilePath));
 
-    this.envConfig = parse(fs.readFileSync(envFilePath));
     //console.log(`********,envFilePath`);
+    /* */
   }
 
   get(key: string): string {
